2d8065b3d3905efde2961ad734f23004fdcb8de7
devyani1512 <148792031+devyani1512@users.noreply.github.com>
2025-07-17 14:21:46 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import os
 from openai import OpenAI
-from calendar_tool import handle_calendar_command
+from calendar_tools import handle_calendar_command
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
